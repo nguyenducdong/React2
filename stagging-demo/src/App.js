2588746860694e39717975dfe814642ab22cbcf0
admin <>
2019-08-06 19:38:34 +0700

    nhitest1cm

--- stagging-demo/src/App.js
+++ stagging-demo/src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
+//
 const withAmount = (currencyComponent) => 
   class Amount extends Component {
     constructor(props){
