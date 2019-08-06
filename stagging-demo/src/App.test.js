a0209c13e0ca0822083ab4a3d244a70d9057e25d
admin <>
2019-08-06 20:15:55 +0700

    update css

--- stagging-demo/src/App.test.js
+++ stagging-demo/src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-//DONGND
+//DONGND change commit
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
